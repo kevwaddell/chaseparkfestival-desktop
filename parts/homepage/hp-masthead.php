2b51966a8fdc7973c55e9103d2a6c746bf385f8e
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-05-31 15:39:53 +0100

    Added styles and images for homepage masthead.

--- parts/homepage/hp-masthead.php
+++ parts/homepage/hp-masthead.php
@@ -1,3 +1,23 @@
 <header id="hp-masthead" class="masthead" role="banner">
+	<div class="mast-bg-imgs">
+		<div class="mast-guitar-bg"></div>
+		<div class="mast-grass-bg"></div>
+	</div>
+	
+	<div class="container">
+		<div class="col-xs-10 col-xs-offset-1">
+			<div class="logo-wrap">
+				<div class="logo"><span class="sr-only"></span></div>
+			</div>
+			<div class="date-location text-center tk-azo-sans-uber">
+				<time class="hp-mast-date dl-item"><span class="bg-col-blue wht-drop-shad">Saturday 6th August 2016</span></time>
+				<div class="hp-mast-location dl-item"><span class="bg-col-orange wht-drop-shad">Whickham ~ Chase park ~ gateshead</span></div>
+				<time class="hp-mast-time dl-item"><span class="bg-col-blue-dk wht-drop-shad">12 - 9pm</span></time>
+			</div>
+		</div>
+		<div class="col-xs-1">
+			<button id="main-nav-btn" class="btn btn-default btn-block"><span class="menu-label txt-col-wht tk-azo-sans-uber">Menu</span><i class="fa fa-bars fa-lg"></i></button>
+		</div>
+	</div>
 	
 </header>
