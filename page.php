6a73a22e2a1ad36de3e037b5a4e7a780c55dd827
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-06-08 09:16:06 +0100

    Add sponsors and social links to all pages.

--- page.php
+++ page.php
@@ -3,9 +3,9 @@
 <main id="main-content">
 <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class(); ?>>
-		<header class="strap-header bg-col-blue-dk txt-col-wht tk-azo-sans-uber">
+		<div class="strip-header bg-col-blue-dk txt-col-wht tk-azo-sans-uber">
 			<div class="container">
-				<h1 class="strap-header"><?php the_title(); ?></h1>
+				<h1><?php the_title(); ?></h1>
 			</div>
 		</header>
 	
@@ -18,5 +18,7 @@
 <?php endwhile; ?>
 <?php endif; ?>
 </main><!-- #main-content -->
+
+<?php get_template_part( 'parts/sections/section', 'social' ); ?>
 
 <?php get_footer(); ?>
