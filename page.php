c40a19d5e4072735c5b2d2acbf911b277b95cbc2
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-06-20 15:33:33 +0100

    Fix strip header.

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 <main id="main-content">
 <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class(); ?>>
-		<div class="strip-header bg-col-blue-dk txt-col-wht tk-azo-sans-uber">
+		<header class="strip-header bg-col-blue-dk txt-col-wht tk-azo-sans-uber">
 			<div class="container">
 				<h1><?php the_title(); ?></h1>
 			</div>
