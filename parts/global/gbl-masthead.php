884ca646c5895227ed76e34d3419f76128bb51dc
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-06-22 16:01:31 +0100

    A few fixes to the home page and global masthead.

--- parts/global/gbl-masthead.php
+++ parts/global/gbl-masthead.php
@@ -5,16 +5,16 @@
 	</div>
 	
 	<div class="container">
+		
 		<div class="row">
 			<div class="col-xs-10 col-xs-offset-1">
 				<div class="logo-wrap">
 					<a href="<?php echo get_option('home'); ?>/" class="logo"><span class="sr-only"><?php bloginfo('name'); ?></span></a>
 				</div>
 			</div>
-			<div class="col-xs-1">
-				<button id="main-nav-btn" class="btn btn-default btn-block"><span class="menu-label txt-col-wht tk-azo-sans-uber">Menu</span><i class="fa fa-bars fa-lg"></i></button>
-			</div>
 		</div>
+		
+		<button id="main-nav-btn" class="btn btn-default btn-block"><span class="menu-label txt-col-wht tk-azo-sans-uber">Menu</span><i class="fa fa-bars fa-lg"></i></button>
 	</div>
 	
 	<div class="breadcrumbs bg-col-blue tk-azo-sans-uber wht-drop-shad" typeof="BreadcrumbList" vocab="http://schema.org/">
--- parts/homepage/hp-masthead.php
+++ parts/homepage/hp-masthead.php
@@ -13,21 +13,16 @@ $gbl_tickets_url = get_field('gbl_tickets_url', 'options');
 	</div>
 	
 	<div class="container">
-		<div class="row">
-			<div class="col-xs-10 col-xs-offset-1">
-				<div class="logo-wrap">
-					<div class="logo"><span class="sr-only"><?php bloginfo('name'); ?></span></div>
-				</div>
-				<div class="date-location text-center tk-azo-sans-uber">
-					<time class="hp-mast-date dl-item"><span class="bg-col-blue wht-drop-shad"><?php echo $fest_date; ?></span></time>
-					<div class="hp-mast-location dl-item"><span class="bg-col-orange wht-drop-shad"><?php echo $fest_location; ?></span></div>
-					<time class="hp-mast-time dl-item"><span class="bg-col-blue-dk wht-drop-shad"><?php echo $fest_time; ?></span></time>
-				</div>
+			<div class="logo-wrap">
+				<div class="logo"><span class="sr-only"><?php bloginfo('name'); ?></span></div>
 			</div>
-			<div class="col-xs-1">
-				<button id="main-nav-btn" class="btn btn-default btn-block"><span class="menu-label txt-col-wht tk-azo-sans-uber">Menu</span><i class="fa fa-bars fa-lg"></i></button>
+			<div class="date-location text-center tk-azo-sans-uber">
+				<time class="hp-mast-date dl-item"><span class="bg-col-blue wht-drop-shad"><?php echo $fest_date; ?></span></time>
+				<div class="hp-mast-location dl-item"><span class="bg-col-orange wht-drop-shad"><?php echo $fest_location; ?></span></div>
+				<time class="hp-mast-time dl-item"><span class="bg-col-blue-dk wht-drop-shad"><?php echo $fest_time; ?></span></time>
 			</div>
-		</div>
+			
+			<button id="main-nav-btn" class="btn btn-default btn-block"><span class="menu-label txt-col-wht tk-azo-sans-uber">Menu</span><i class="fa fa-bars fa-lg"></i></button>
 		
 		<a href="<?php echo $gbl_tickets_url; ?>" target="_blank" class="btn btn-default btn-block btn-lg book-tickets-btn-lg tk-azo-sans-uber">Book Your Tickets</a>
 		
