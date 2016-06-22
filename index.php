abfccc1f80db07e9fd49f1f7b5542ccd7a092b49
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-06-22 09:43:03 +0100

    Message added.

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>	
 
-<main id="main-content">
+<div class="strip-header bg-col-blue-dk txt-col-wht tk-azo-sans-uber">
+	<div class="container">
+		<h1><?php echo get_the_title( get_option('page_for_posts')); ?></h1>
+	</div>
+</div>
+
 <?php if ( have_posts() ): ?>
+<main id="main-content">
 	<section id="blog-posts">
 	<?php while ( have_posts() ) : the_post(); ?>
 		<article <?php post_class(); ?>>
@@ -10,9 +16,14 @@
 				<?php the_content(); ?>
 			</div>
 		</article>
-<?php endwhile; ?>
+		<?php endwhile; ?>
 	</section>
+<?php else: ?>
+<main id="main-content" class="notes-bg-orange"> 
+	<?php get_template_part( 'parts/messages/blog', 'message' ); ?>
+	
 <?php endif; ?>
 </main><!-- #main-content -->
+
 
 <?php get_footer(); ?>
