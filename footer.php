827190a96747c2507a61a78d021a15e6531c8ac3
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-11-29 15:55:25 +0000

    Add Recite

--- footer.php
+++ footer.php
@@ -4,10 +4,33 @@
 		<?php } ?>
 		
 		<?php get_template_part( 'parts/global/gbl', 'footer' ); ?>
-			
+		
+		<?php if ($_SERVER['SERVER_NAME']=='www.chaseparkfestival.co.uk') { ?>
+		<div class="reciteme">Enable Recite</div>
+		<script type="text/javascript">
+	    jQuery(".reciteme").click(function(i,e){
+	        loadService();
+	        return false;
+	    });
+	
+		</script>
+		<?php } ?>
 	</div><!-- .cpf-wrapper end -->
 	
 	<?php wp_footer(); ?>
+	
+	<?php if ($_SERVER['SERVER_NAME']=='www.chaseparkfestival.co.uk') { ?>
+	<script type="text/javascript">
+	    var serviceUrl = "//api.reciteme.com/asset/js?key=";
+	    var serviceKey = "73c7291e2150423df6c8bdfea2c7e0e575c34c45";
+	    var options = {};  // Options can be added as needed
+	    var autoLoad = false;
+	    var enableFragment = "#reciteEnable";
+	
+	    var loaded=[],frag=!1;window.location.hash===enableFragment&&(frag=!0);function loadScript(c,b){var a=document.createElement("script");a.type="text/javascript";a.readyState?a.onreadystatechange=function(){if("loaded"==a.readyState||"complete"==a.readyState)a.onreadystatechange=null,void 0!=b&&b()}:void 0!=b&&(a.onload=function(){b()});a.src=c;document.getElementsByTagName("head")[0].appendChild(a)}function _rc(c){c+="=";for(var b=document.cookie.split(";"),a=0;a<b.length;a++){for(var d=b[a];" "==d.charAt(0);)d=d.substring(1,d.length);if(0==d.indexOf(c))return d.substring(c.length,d.length)}return null}function loadService(c){for(var b=serviceUrl+serviceKey,a=0;a<loaded.length;a++)if(loaded[a]==b)return;loaded.push(b);loadScript(serviceUrl+serviceKey,function(){"function"===typeof _reciteLoaded&&_reciteLoaded();"function"==typeof c&&c();Recite.load(options);Recite.Event.subscribe("Recite:load",function(){Recite.enable()})})}"true"==_rc("Recite.Persist")&&loadService();(autoLoad&&"false"!=_rc("Recite.Persist")||frag)&&loadService();
+	
+	</script>
+<?php } ?>
 
 	</body>
 </html>
