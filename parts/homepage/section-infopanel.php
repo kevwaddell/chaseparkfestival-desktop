5a1e9ce44518d6924133963d751c0ea7933f70f6
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-03-01 11:45:23 +0000

    Update styles for 2018 - part 5

--- parts/homepage/section-infopanel.php
+++ parts/homepage/section-infopanel.php
@@ -6,7 +6,7 @@ $hp_info_txt = get_field('hp_info_txt', 'options');
 
 <section class="col-panel bg-col-blue-dk">
 	<div class="container rel">
-		<div class="panel-imgs offset-imgs" style="background-image: url(<?php bloginfo('stylesheet_directory'); ?>/_/img/hp-panel-imgs.png)"></div>
+		<div class="panel-imgs offset-imgs" style="background-image: url(<?php bloginfo('stylesheet_directory'); ?>/_/img/hp-panel-imgs-2018.png)"></div>
 		<div class="row">
 			<div class="col-xs-7 col-xs-offset-5">
 				<div class="panel-txt txt-col-wht bold txt-cols-2">
