91112daa5a1206718efe9902e51ed5684c2a1c67
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-03-01 12:26:14 +0000

    Update styles for 2018 - part 8

--- parts/panels/sponsor-enquiries.php
+++ parts/panels/sponsor-enquiries.php
@@ -1,16 +1,16 @@
 <?php  
-$gbl_contact_name  = get_field('gbl_ae_contact_name', 'options');
-$gbl_contact_email = get_field('gbl_ae_contact_email', 'options');
-$gbl_contact_telephone = get_field('gbl_ae_contact_telephone', 'options');
+$contact_name  = get_field('gbl_sp_contact_name', 'options');
+$contact_email = get_field('gbl_sp_contact_email', 'options');
+$contact_telephone = get_field('gbl_sp_contact_telephone', 'options');
 $contact_page = get_page_by_title("Contact us");
 ?>
 <div class="info-panel panel-blue mag-bot-30">
 <!-- 	<div class="sponsor-icon-img"></div> -->
 	<h3 class="txt-col-orange text-center tk-azo-sans-uber">Sponsorship enquiries</h3>
 	<div class="panel-inner">
-		<div class="contact-info-sml panel-name text-center txt-col-wht tk-azo-sans-uber"><?php echo $gbl_contact_name; ?></div>
-		<div class="contact-info-sml panel-email text-center bold"><a href="mailto:<?php echo $gbl_contact_email; ?>"><?php echo $gbl_contact_email; ?></a></div>
-		<div class="contact-info-sml panel-tel text-center txt-col-wht bold"><?php echo $gbl_contact_telephone; ?></div>
+		<div class="contact-info-sml panel-name text-center txt-col-wht tk-azo-sans-uber"><?php echo $contact_name; ?></div>
+		<div class="contact-info-sml panel-email text-center bold"><a href="mailto:<?php echo $contact_email; ?>"><?php echo $gbl_contact_email; ?></a></div>
+		<div class="contact-info-sml panel-tel text-center txt-col-wht bold"><?php echo $contact_telephone; ?></div>
 	</div>
 	<a href="<?php echo get_permalink($contact_page->ID); ?>" class="contact-btn btn btn-default btn-block tk-azo-sans-uber">Use our online form</a>
 </div>
