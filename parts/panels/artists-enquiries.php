42a8b4e964bdb7070e7dd69c5e5585a94c67e24f
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-03-08 15:53:42 +0000

    Change artist enquiries panel to sponsorship enquiries.

--- parts/panels/artists-enquiries.php
+++ parts/panels/artists-enquiries.php
@@ -5,7 +5,7 @@ $gbl_ae_contact_telephone = get_field('gbl_ae_contact_telephone', 'options');
 ?>
 
 <div class="info-panel panel-blue">
-	<h3 class="panel-header tk-azo-sans-uber text-center txt-col-orange">Artist Enquiries</h3>
+	<h3 class="panel-header tk-azo-sans-uber text-center txt-col-orange">Sponsorship Enquiries</h3>
 	<div class="panel-inner">
 		<div class="contact-info panel-name text-center txt-col-wht tk-azo-sans-uber"><?php echo $gbl_ae_contact_name; ?></div>
 		<div class="contact-info panel-email text-center bold"><a href="mailto:<?php echo $gbl_ae_contact_email; ?>"><?php echo $gbl_ae_contact_email; ?></a></div>
